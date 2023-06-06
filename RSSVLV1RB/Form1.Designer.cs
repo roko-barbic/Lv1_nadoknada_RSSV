87747fdfcb1a415222bc8d05d3262d21e172cb45
roko-barbic <rokobarosk12345@gmail.com>
2023-06-06 20:57:08 +0200

    prvi commit

---
+++ RSSVLV1RB/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace RSSVLV1RB
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_start_stop = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btn_start_stop
+            // 
+            this.btn_start_stop.Location = new System.Drawing.Point(166, 167);
+            this.btn_start_stop.Name = "btn_start_stop";
+            this.btn_start_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_start_stop.TabIndex = 1;
+            this.btn_start_stop.Text = "button1";
+            this.btn_start_stop.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 84);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(141, 84);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 217);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_start_stop);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_start_stop;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+
+        private Label label2;
+        private Label label3;
+        private Label label4;
+
+    }
+}
+
---
+++ RSSVLV1RB/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSSVLV1RB
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer t;
+        private int sati;
+        private int minute;
+        private bool alarmIsSet = false;
+        private bool alarmIsInvoked = false;
+         public Form1()
+         {
+             InitializeComponent();
+             //Kreiranje timer-a s periodom od 1000 ms
+             t = new System.Timers.Timer(1000);
+
+             //Dodavanje događaja timer-u; izvršava se periodno
+             t.Elapsed += new System.Timers.ElapsedEventHandler(vrijeme);
+             t.Start();
+             this.btn_start_stop.Click += new System.EventHandler(this.btn_set_alarm);
+             btn_start_stop.Text = "Postavi alarm";
+             label2.Text = "Sati:";
+             label3.Text = "Minute:";
+             label4.Text = "";
+
+         }
+
+         private void vrijeme(object s, EventArgs e)
+         {
+             Invoke((MethodInvoker)delegate //Anonimna metoda
+             {
+
+                 label1.Text = DateTime.Now.ToLongTimeString();
+
+                 if (alarmIsSet && alarmIsInvoked== false)
+                 {
+                     if (DateTime.Now.Hour== sati && DateTime.Now.Minute== minute)
+                     {
+                         Console.Beep(500,5000);
+                         //MessageBox.Show("Alarm triggered!");
+                         label4.Text = "";
+                         alarmIsInvoked = true;
+                         //alarmIsSet = false;
+                     }
+                 }
+             });
+         }
+
+         private void btn_set_alarm(object sender, EventArgs e)
+         {
+             sati = (int)numericUpDown1.Value;
+             minute = (int)numericUpDown2.Value;
+             label4.Text = "Alarm je postavljen u " + sati + " : " + minute;
+             alarmIsSet = true;
+             alarmIsInvoked = false;
+         }
+
+    }
+}
